1744ba85355c70449784bfc4a727fda5408e3d9a
mansf843 <trentonknight@gmail.com>
2010-10-05 23:54:21 -0400

    #DelouthMansfield-Assn4-Functions.cpp# having issues with typedef

--- DelouthMansfield-Assn4-Functions.cpp
+++ DelouthMansfield-Assn4-Functions.cpp
@@ -2,36 +2,48 @@
 #include <iomanip>
 #include <string>
 #include <cstddef>
+#include <ctime>
 
 using namespace std;
 
- 
-int getRandomNum(**GETrandNUM_ONE,**GETrandNUM_TWO);
-//all sort functions to call by pointer to functions
-int bubbleSort(**randNUM);
-int insertionSort(**randNUM);
-int mergeSort(**randNUM);
-int quickSort(**randNUM);
-//end functions called by pointer? I think?
-int verifySorting(**VERIFYrandNUM_ONE, **VERIFYrandNUM_TWO);
-clockTime();
-displayResults();
 
-const int ARRAY_MAX = 5;//will change to 100,000 after it works
-typedef int (*sortPOINTER)(**randARRAY);//pointer to all sort functions
+int getRandomNum(int randNUM_ONE[],int randNUM_TWO[]);
+//All sort functions to call by pointer to functions
+int bubbleSort(int randNUM[]);
+int insertionSort(int randNUM[]);
+int mergeSort(int randNUM[]);
+int quickSort(int randNUM[]);
+//end functions called by pointer? I think?
+int verifySorting(int randNUM_ONE[],int randNUM_TWO[]);
+void clockTime();
+void displayResults();
+
+const int RAND_INT = 100000;//will change to 100,000 after it works
+const int ARRAY_FUNC = 4;
+
+typedef int (*sortPOINTER)(randARRAY[]);
+const sortPOINTER sortPOINTER_ARRAY[ARRAY_FUNC] = {&bubbleSort,
+                                          &insertionSort,
+                                          &mergeSort,
+                                          &quickSort};
+
+const string sortDescriptions[ARRAY_FUNC] = {"BUBBLE_SORT","INSERTION_SORT","MERGE_SORT","QUICK_SORT"};
+
 // main()
-// Main program logic. Handles getting user input 
+// Main program logic. Handles getting user input
 // returns:
 //   0 - No error, program exited normally
-// Calls: 
-// Implemented by: 
+// Calls:
+// Implemented by:
 int main()
 {
   char pickASort_ONE, pickASort_TWO;
-
+  int randNUM[RAND_INT];
+  int randNUM_ONE[RAND_INT];
+  int randNUM_TWO[RAND_INT] = {0};
   sortPOINTER ptrToSortFunctions;//define pointer to sort functions
-  int arrayONE[ARRAY_MAX] = {1,2,3,4,5};//arrays for sorting
-  int arrayTWO[ARRAY_MAX] = {1,2,3,4,5};
+  int arrayONE[RAND_INT];//arrays for sorting
+  int arrayTWO[RAND_INT];
   //allow user to select which sort functions  they want to use, will need to expand this oviously
   cout << "First sort functions?" << endl;
   cin >> pickASort_ONE;
@@ -39,68 +51,66 @@ int main()
   cin >> pickASort_TWO;
 
   //this is just a rough example of a if statement or a switch we will need to expand of course
-  if(pickASort_ONE == "B"){
-         
+  if(pickASort_ONE == 'B'){
+
           ptrToSortFunctions = &bubbleSort;
-	} 
+        }
  else//and so forth to pick which function to call via ptrToSortFunctions for both pickASort_ONE  one and then pickASort_TWO
 
-  getRandomNum(**GETrandNUM_ONE,**GETrandNUM_TWO);//create random array of numbers that match
-  VERIFYrandNUM_ONE = (*ptrToSortFunctions)(**arrayONE);//copy newly sorted array 1 to another array 1
-  VERIFYrandNUM_TWO = (*ptrToSortFunctions)(**arrayTWO);//copy newly sorted array 2 to another array 2
-  int verifySorting(**VERIFYrandNUM_ONE, **VERIFYrandNUM_TWO);//now run copied arrays through verification
-      
+  getRandomNum(randNUM_ONE,randNUM_TWO);//create random array of numbers that match
+  ptrToSortFunctions = sortPOINTER_ARRAY[ARRAY_FUNC];
+
+  verifySorting(randNUM_ONE,randNUM_TWO);//now run copied arrays through verification
+
     return 0;
 }
 // getRandomNum()
-// Generate 100,000 random numbers between 1 and 30,000 and place them into two 100,000-element arrays 
+// Generate 100,000 random numbers between 1 and 30,000 and place them into two 100,000-element arrays
 // Returns:
-//  
-// Implemented by: 
-int getRandomNum(**GETrandNUM_ONE,**GETrandNUM_TWO);
-{
-
-}
+//
+// Implemented by:
+int getRandomNum(int **randNUM_ONE,int **randNUM_TWO){}
 
 
 // bubbleSort()
-// 
+//
 // Returns:
-//  
-// Implemented by: 
-int bubbleSort(**randNUM)
+//
+// Implemented by:
+int bubbleSort(int **randNUM)
 {
 
 }
 
 
 // insertionSort()
-// 
+//
 // Returns:
-//  
-// Implemented by: 
-int insertionSort(**randNUM)
+//
+// Implemented by:
+int insertionSort(int **randNUM)
 {
 
 }
 
 
 // mergeSort()
-// 
+//
 // Returns:
-//  
-// Implemented by: 
-int mergeSort(**randNUM)
+//
+// Implemented by:
+int mergeSort(int **randNUM)
 {
+  static int tempList[RAND_INT];
 
 }
 
 // quickSort()
-// 
+//
 // Returns:
-//  
-// Implemented by: 
-int quickSort(**randNUM)
+//
+// Implemented by:
+int quickSort(int **randNUM)
 {
 
 }
@@ -108,28 +118,28 @@ int quickSort(**randNUM)
 // verifySorting()
 //  verifies the sorts ran correctly
 // Returns:
-//  
-// Implemented by: 
-int verifySorting(**VERIFYrandNUM_ONE, **VERIFYrandNUM_TWO){
+//
+// Implemented by:
+int verifySorting(int **randNUM_ONE,int **randNUM_TWO){
 
 }
 
 // clockTime()
 // calculate how long the sort took
 // Returns:
-//  
-// Implemented by: 
-clockTime()
+//
+// Implemented by:
+void clockTime()
 {
 
 }
 
 // displayResults()
-// 
+//
 // Returns:
-//  
-// Implemented by: 
-displayResults()
+//
+// Implemented by:
+void displayResults()
 {
 
 }
