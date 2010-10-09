7a9a29c8282a10c3a93d4bdd2f5ae5c1ba748c1f
mansf843 <trentonknight@gmail.com>
2010-10-08 20:14:04 -0400

    update

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -6,69 +6,133 @@
 
 using namespace std;
 
-void mergesort(int list[],int tempArray[],int lo,int hi);
-void merge(int list[],int tempArray[],int lo,int mid,int hi);
+void merge(int numbers[], int temp[], int left, int mid, int right);
 
-int main()
+
+void mergesort(int numbers[], int temp[], int left, int right)
+
 {
-    int list[10] = {1,19,2,10,6,5,4,7,9,8};
-    int tempArray[10] = {0};
-    for(int a = 0; a < 10; a++){
-        cout << "before: " << list[a] << endl;
-}
-    mergesort(list,tempArray,0,10);
+  int mid;
 
-    for(int a = 0; a < 10; a++){
-        cout << "after: " << list[a] << endl;}
+  if (right > left)
+
+  {
+
+    mid = (right + left) / 2;
+
+    mergesort(numbers, temp, left, mid);
+
+    mergesort(numbers, temp, mid+1, right);
 
 
-    return 0;
-}
+    merge(numbers, temp, left, mid+1, right);
 
-void mergesort(int list[],int tempArray[],int lo,int hi)
-{
-  int mid = 0;
-
-    if (lo<hi)
-    {
-        mid=(lo+hi)/2;
-        mergesort(list,tempArray,lo,mid);
-        mergesort(list,tempArray,mid+1,hi);
-        merge(list,tempArray,lo,mid+1,hi);
-    }
-}
-
-// Straightforward variant
-void merge(int list[],int tempArray[],int lo,int mid,int hi)
-{
-  int left = lo;
-  int right = mid + 1;
-  int temp = lo;
-  tempArray[mid] = list[mid];
-
-  while((left <= mid) && (right <=hi)){
-    if(tempArray[left] <= tempArray[right]){
-      list[temp] = tempArray[left];
-      left++;
-    }
-    else{
-      list[temp] = tempArray[right];
-      right++;
-    }
-    temp++;
   }
-  //loop 2 add elements left side
-  while(left <= mid){
-    list[temp] = tempArray[left];
-    temp++;
-    left++;
-  }
-  //loop 3 add elements right
-  while(right <= hi)
+
+}
+
+
+
+void merge(int numbers[], int temp[], int left, int mid, int right)
+
+{
+
+  int i, left_end, num_elements, tmp_pos;
+
+
+  left_end = mid - 1;
+
+  tmp_pos = left;
+
+  num_elements = right - left + 1;
+
+
+
+  while ((left <= left_end) && (mid <= right))
+
+  {
+
+    if (numbers[left] <= numbers[mid])
+
     {
-      list[temp] = tempArray[right];
-      temp++;
-      right++;
-    }  
- 
+
+      temp[tmp_pos] = numbers[left];
+
+      tmp_pos = tmp_pos + 1;
+
+      left = left +1;
+
+    }
+
+    else
+
+    {
+
+      temp[tmp_pos] = numbers[mid];
+
+      tmp_pos = tmp_pos + 1;
+
+      mid = mid + 1;
+
+    }
+
+  }
+
+
+
+  while (left <= left_end)
+
+  {
+
+    temp[tmp_pos] = numbers[left];
+
+    left = left + 1;
+
+    tmp_pos = tmp_pos + 1;
+
+  }
+
+  while (mid <= right)
+
+  {
+
+    temp[tmp_pos] = numbers[mid];
+
+    mid = mid + 1;
+
+    tmp_pos = tmp_pos + 1;
+
+  }
+
+
+
+  for (i=0; i <= num_elements; i++)
+
+  {
+
+    numbers[right] = temp[right];
+
+    right = right - 1;
+
+  }
+
+}
+
+int main(){
+  int numbers[10] = {1,10,9,8,5,6,2,3,4,7};
+  int temp [10] = {0};
+  int a, b;
+
+  for(a = 0; a < 10; a++){
+
+  cout << numbers[a] << endl;
+  }
+  cout << endl;
+
+  mergesort(numbers,temp,0,9);
+
+  for(b = 0; b < 10; b++){
+
+  cout << numbers[a] << endl;
+  }
 }
