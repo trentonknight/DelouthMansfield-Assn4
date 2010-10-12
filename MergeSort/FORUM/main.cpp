94050ac080c3fa9ea79563996a15c59668932318
trentonknight <trentonknight@gmail.com>
2010-10-11 20:42:55 -0400

    updated last weeks forum code for merge sort ver 2

--- MergeSort/FORUM/main.cpp
+++ MergeSort/FORUM/main.cpp
@@ -9,30 +9,25 @@ using namespace std;
 void merge(int numbers[], int temp[], int left, int mid, int right);
 
 
-void mergesort(int numbers[], int temp[], int left, int right)
+void mergesort(int numbers[], int temp[], int left, int right,int& count,int& m)
 
 {
   int mid;
+  
+  count++;
+  
 
   if (right > left)
 
   {
    
     mid = (right + left) / 2;
-    cout << "INDICES: " << "["<<left <<"] => ["<< mid <<"] => [" << right << "]"<< endl;
-    cout << "Call 1: " << "["<<numbers[left] <<"] => ["<< numbers[mid] <<"] <= [" << numbers[right] << "]"<< endl;
+    cout << "INDICES: " << "["<<left <<"] => ["<< mid <<"] / [" <<mid+1<<"] <= ["<< right << "]"<< endl;
+    cout << "MergeSort: " << count << " ["<<numbers[left] <<"] => ["<< numbers[mid] <<"] / ["<<numbers[mid+1]<<"] <= [" << numbers[right] << "]"<< endl;
 
-   int b = mid+1;
-   cout << "RIGHT: ";
-   while(b <= right){
-  
-   cout << "[" << numbers[b] << "]";
-   b++;
-}
-  cout << endl;
-  
-   int c = mid;
-   cout << "LEFT: ";
+
+int c = mid;
+   cout << "LEFT SORT: ";
    while(c >= left){
   
    cout << "[" << numbers[c] << "]";
@@ -41,12 +36,26 @@ void mergesort(int numbers[], int temp[], int left, int right)
   cout << endl;
   
   
-  
- 
 
-    mergesort(numbers, temp, left, mid);
-    mergesort(numbers, temp, mid +1, right);
+   int b = mid+1;
+   cout << "RIGHT SORT: ";
+   while(b <= right){
+  
+   cout << "[" << numbers[b] << "]";
+   b++;
+}
+    cout << endl;
+    mergesort(numbers, temp, left, mid,count,m);
+    mergesort(numbers, temp, mid +1, right,count,m);
     merge(numbers, temp, left, mid+1, right);
+    
+    cout << "After Merge "<< m++ << " " << endl;
+    
+ for(int a = 0; a < 10; a++){
+
+   cout << "[" << numbers[a] << "]";
+  }
+  cout << endl;
 
 }
 
@@ -79,6 +88,8 @@ void merge(int numbers[], int temp[], int left, int mid, int right)
     {
 
       temp[tmp_pos] = numbers[left];
+      
+      cout << "LEFT MERGE: " << numbers[left] << " TO: " << left << endl;
 
       tmp_pos = tmp_pos + 1;
 
@@ -123,6 +134,8 @@ void merge(int numbers[], int temp[], int left, int mid, int right)
     mid = mid + 1;
 
     tmp_pos = tmp_pos + 1;
+    
+    cout << "RIGHT MERGE: " << numbers[mid -1] << " TO: " << mid +1<< endl;
 
   }
 
@@ -144,6 +157,8 @@ int main(){
   int numbers[10] = {50,16,64,37,24,75,92,41};
   int temp [10] = {0};
   int a, b;
+  int count = 0;
+  int m = 1;
 
  cout << "Begin Merge Sort: " << endl;
 
@@ -153,7 +168,7 @@ int main(){
   }
   cout << endl;
 
-  mergesort(numbers,temp,0,7);
+  mergesort(numbers,temp,0,7,count,m);
   
 cout << "Completed Merge Sort: " << endl;
 for(b = 0; b < 10; b++){
